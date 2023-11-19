c6c61cb3e31832fe07a3b408f5de7d0b99a1c31b
Joel <joel.viana@audora.com.br>
2023-11-19 09:16:26 -0300

    creating microservices course

---
+++ kuber-courses/src/test/java/com/joel/springcloud/mscv/course/KuberCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.springcloud.mscv.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
